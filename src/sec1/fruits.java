959dfa78349fa4f4e439121065af5129abdf191e
somnath <pandharpure21@gmail.com>
2022-10-22 21:28:54 +0530

    first commit.......

---
+++ src/sec1/fruits.java
@@ -0,0 +1,31 @@
+package sec1;
+
+
+public class fruits {
+	
+	public static void main (String[]args) {
+	
+	//1.variable Declaration
+	String student_first_name; 
+	String student_last_name;
+	int Roll_No; 
+	char studentgrade;
+	float student_percentage ;
+	
+	student_first_name="priyanka";
+	student_last_name="pandharpure";
+	Roll_No=20;
+	studentgrade='A';
+	student_percentage=50f;
+	
+	System.out.println(student_first_name);
+	System.out.println(student_last_name);
+	System.out.println(Roll_No);
+	System.out.println(studentgrade);
+	System.out.println(student_percentage);
+	
+	
+	
+	}
+
+}
---
+++ src/sec2/vegitables.java
@@ -0,0 +1,25 @@
+package sec2;
+
+import java.security.PublicKey;
+
+public class vegitables {
+
+public static void main (String[]args) {
+	
+	int a =12;
+	int b=20;
+	 int c=a+b;
+ 
+System.out.println(c);		
+}
+
+
+
+
+
+
+
+
+
+
+}
